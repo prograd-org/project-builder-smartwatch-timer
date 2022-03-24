9aca1bf1790fe06daea6cbbdae218fe83412c047
Nile Krupa Sheel <krupasheel@gmail.com>
2022-03-24 23:59:44 +0530

    updated

--- app.js
+++ app.js
@@ -1,215 +1,123 @@
-function displayTime() {
-    var currTime = new Date();
-    var h = currTime.getHours();
-    var m = currTime.getMinutes();
-  
-    if (h == 24) {
-      h = 0;
-    } else if (h > 12) {
-      h = h - 0;
-    }
-    if (h < 10) {
-      h = "0" + h;
-    }
-    if (m < 10) {
-      m = "0" + m;
-    }
-  
-    var headTimeShow = document.getElementById("headTime");
-    headTimeShow.innerHTML = h + ":" + m;
-  }
-  displayTime();
-  
-  
-  
-  
-   document.querySelector("#msg").onclick = function () {
-      
-      var mainContent = document.querySelector("#main");
-      mainContent.innerHTML = "";
-      var msgContainer = document.createElement("div");
-      msgContainer.id = "msg-cont";
-      msgContainer.innerHTML = `
-         <div id="java" onclick="showText('java')">Java</div>
-        <div id="html" onclick="showText('html')">Html</div>
-        <div id="css" onclick="showText('css')">css</div>`;
-      mainContent.appendChild(msgContainer);
-    };
-    
-    function showText(data) {
-      var msgBox = document.querySelector("#msg-cont");
-      msgBox.innerHTML = "";
-      var msg = document.createElement("p");
-      msg.id = "text";
-      switch (data) {
-        case "java":
-          msg.innerHTML =
-            "Java is a high level, robust, object-oriented and secure programming language.";
-          break;
-        case "html":
-          msg.innerHTML =
-            " HTML stands for hypertext markup language and it is used to create pages which are rendered over the web.";
-          break;
-        case "css":
-          msg.innerHTML =
-            "CSS stands for Cascading Style Sheets used to design a document written in markup language. ";
-          break;
-      }
-      msgBox.appendChild(msg);
-    }
-  
-  
-  
-  
-  document.querySelector("#clock").onclick = function () {
-    var mainContent = document.querySelector("#main");
-    mainContent.innerHTML = "";
-    var clockContent = document.createElement("div");
-    clockContent.id = "clock-item";
-    clockContent.innerHTML = `
-      
-      <h1 class="timer">
-          <span id="min">00</span>
-          <span id="sec">00</span>
-          <span id="m-sec">00</span>
-      </h1>
-      <div id="clock-icons">
-          <a id="reset-btn"><i class="fa fa-refresh" aria-hidden="true"></i></a>
-          <a id="stop-btn"><i class="fa fa-pause" aria-hidden="true"></i></a>
-          <a id="start-btn"><i class="fas fa-square-full"></i></a>
-        </div>
-      `;
-    mainContent.appendChild(clockContent);
-  
-    let count = 0;
-    let minutes = 0;
-    let seconds = 0;
-    let interval = 0;
-    let milliseconds = 0;
-    let timerRunning = false;
-  
-    let startTimer = () => {
-      count++;
-      minutes = Math.floor(count / 100 / 60);
-      seconds = Math.floor(count / 100 - minutes * 60);
-      milliseconds = Math.floor(count - seconds * 100 - minutes * 6000);
-  
-      document.querySelector("#min").innerText = loadingzero(minutes);
-      document.querySelector("#sec").innerText = loadingzero(seconds);
-      document.querySelector("#m-sec").innerText = loadingzero(milliseconds);
-    };
-  
-    let startBtn = document.querySelector("#start-btn");
-    startBtn.addEventListener("click", function () {
-      if (!timerRunning) {
-        interval = setInterval(startTimer, 10); // start the timer
-        timerRunning = true;
-      }
-    });
-  
-    
-    let stoptBtn = document.querySelector("#stop-btn");
-    stoptBtn.addEventListener("click", function () {
-      clearInterval(interval);
-    });
-  
-    let resetBtn = document.querySelector("#reset-btn");
-    resetBtn.addEventListener("click", function () {
-      clearInterval(interval);
-       count = 0;
-       minutes = 0;
-       seconds = 0;
-       interval = 0;
-       milliseconds = 0;
-       timerRunning = false;
-  
-      document.querySelector("#min").innerText = "00";
-      document.querySelector("#sec").innerText = "00";
-      document.querySelector("#m-sec").innerText = "00";
-    });
-  
-    
-  
-    let loadingzero = (time) => {
-      if (time <= 9) {
-        return "0" + time;
-      } else {
-        return time;
-      }
-    };
-  };
-  
-  var mainContent = document.querySelector("#main");
-  mainContent.innerHTML = `
-    <h2 class="t2" id="t2"></h2>
-    <p class="day" id="day"></p>
-    <p class="date" id="date"></p>
-    `;
-  function renderTime() {
-    var mydate = new Date();
-    var year = mydate.getFullYear();
-    if (year < 1000) {
-      year += 1900;
-    }
-    var day = mydate.getDay();
-    var month = mydate.getMonth();
-    var daym = mydate.getDate();
-    var monthArr = new Array(
-      "Jan",
-      "Feb",
-      "Mar",
-      "April",
-      "May",
-      "June",
-      "July",
-      "Aug",
-      "Sep",
-      "Oct",
-      "Nov",
-      "Dec"
-    );
-    var dayArr = new Array(
+function _(id) {
+  return document.querySelector(id);
+}
+// Time
+setInterval(() => {
+  let today = new Date();
+  let day = [
       "Sunday",
       "Monday",
-      "wednesday",
+      "Tuesday",
+      "Wednesday",
       "Thursday",
       "Friday",
-      "Saturday"
-    );
-  
-    // Time
-    var currTime = new Date();
-    var h = currTime.getHours();
-    var m = currTime.getMinutes();
-    var s = currTime.getSeconds();
-  
-    if (h == 24) {
-      h = 0;
-    } else if (h > 12) {
-      h = h - 0;
-    }
-    if (h < 10) {
-      h = "0" + h;
-    }
-    if (m < 10) {
-      m = "0" + m;
-    }
-    if (s < 10) {
-      s = "0" + s;
-    }
-  
-    var showtime = document.getElementById("t2");
-    showtime.innerHTML = h + ":" + m + ":" + s;
-  
-    var showday = document.getElementById("day");
-    showday.innerHTML = "" + dayArr[day] + "";
-  
-    var showdate = document.getElementById("date");
-    showdate.innerHTML = daym + " " + "" + monthArr[month] + " " + "" + year;
-  
-    setTimeout("renderTime()", 1000);
+      "Saturday",
+  ];
+  _("#day").textContent = `${day[today.getDay()]}`;
+  _("#timeS").textContent = `${String(
+    today.getHours()
+  ).padStart(2, "0")}:${String(today.getMinutes()).padStart(2, "0")}`;
+  _("#timeB").textContent = `${String(
+    today.getHours()
+  ).padStart(2, "0")}:${String(today.getMinutes()).padStart(2, "0")}`;
+}, 1000);
+
+// Messages 
+let msgList = [
+  "Java is a high-level, class-based, object-oriented programming language that is designed to have as few implementation dependencies as possible.",
+  "Cascading Style Sheets is a style sheet language used for describing the presentation of a document written in a markup language such as HTML.",
+  "The HyperText Markup Language, or HTML is the standard markup language for documents designed to be displayed in a web browser.",
+  "JavaScript, often abbreviated as JS, is a programming language that conforms to the ECMAScript specification.",
+  "React is a free and open-source front-end JavaScript library for building user interfaces or UI components. It is maintained by Facebook and a community of individual developers and companies."
+];
+
+let selectMsg = (msgId) => {
+  let msg = _("#showMsg");
+  msg.style.display = "block";
+  msg.textContent = msgList[msgId - 1];
+};
+
+// Media Player
+let smsState, musicState, watchState;
+smsState = musicState = watchState = false;
+
+function selectScreen(scr) {
+  setOff();
+  let icons = _(".icons").childNodes;
+  switch (scr) {
+      case 0:
+          _(".timeB").style.display = "flex";
+          break;
+      case 1:
+          _(".sms").style.display = "flex";
+          icons[scr].style.opacity = 1;
+          _("#showMsg").style.display = "none";
+          break;
+      case 3:
+          _(".media").style.display = "flex";
+          icons[scr].style.opacity = 1;
+          break;
+      case 5:
+          _(".timer").style.display = "flex";
+          icons[scr].style.opacity = 1;
+          break;
   }
-  
-  window.onload = () => {
-    renderTime();
-  };
+}
+
+function setOff() {
+  let icons = _(".icons").childNodes;
+  icons[1].style.opacity = 0.5;
+  icons[3].style.opacity = 0.5;
+  icons[5].style.opacity = 0.5;
+  _(".timeB").style.display = "none";
+  _(".sms").style.display = "none";
+  _(".media").style.display = "none";
+  _(".timer").style.display = "none";
+}
+
+function selectSong(sid) {
+  _("#song").src = `./songs/s${sid}.mp3`;
+  _("#simg").src = `./imgs/s${sid}.png`;
+}
+
+// Timer
+let sec, min, hrs, inte, pid = 0;
+sec = min = hrs = "0";
+
+function timer(id) {
+  switch (id) {
+      case 1:
+          pid = id;
+          sec = min = hrs = "0";
+          _(".csec").textContent = _(".cmin").textContent = _(".chrs").textContent = "00";
+          _(".prev").textContent = "00:00:00";
+          break;
+      case 2:
+          if (pid != id) {
+              pid = id;
+              inte = setInterval(() => {
+                  sec++;
+                  if (sec < 100) {
+                      _(".csec").textContent = (sec < 10) ? `0${sec}` : sec;
+                  } else {
+                      sec = "00";
+                      min++;
+                      if (min < 100) {
+                          _(".cmin").textContent = (min < 10) ? `0${min}` : min;
+                      } else {
+                          min = "00";
+                          _(".cmin").textContent = (min < 10) ? `0${min}` : min;
+                          hrs++
+                          _(".chrs").textContent = (hrs < 10) ? `0${hrs}` : hrs;
+                      }
+                  }
+              }, 10);
+          }
+          break;
+      case 3:
+          pid = id;
+          clearInterval(inte);
+          _(".prev").textContent = `${(hrs < 10) ? `0${hrs}` : hrs}:${(min < 10) ? `0${min}` : min}:${(sec < 10) ? `0${sec}` : sec}`;
+          break;
+  }
+}
